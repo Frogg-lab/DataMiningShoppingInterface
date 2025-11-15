cb4f88eaeb6162483a495acf4e9a8cca05b1c061
Frogg-Lab <nickwasserman12@gmail.com>
2025-11-15 05:50:29 -0500

    implemented eclat

--- src/algorithms/eclat.py
+++ src/algorithms/eclat.py
@@ -1,12 +1,17 @@
 import pandas as pd
-import preprocessing
+from preprocessing import clean_data
 import formulas
 from itertools import combinations
+import time
+import math
+from memory_profiler import memory_usage
 
-data = pd.read_csv('../../data/sample_transactions.csv')
+
+
 
 
 def to_vertical(data):
+
     one_items = set()
     all_rows = data['items'].tolist()
     new_df = pd.DataFrame(columns=['Item', 'Customers'])
@@ -34,14 +39,83 @@ def to_vertical(data):
         new_df.at[counter, 'Customers'] = bought
         counter+=1
     
-    
     return new_df
 
 
+def eclat(data, minimum_support=0.2, minimum_confidence=0.5):
+    start_time = time.time()
+    data = to_vertical(data)
+    supported_sets = {}
+    frequent_items = dict()
+    transaction_count = set()
+    n_size = 1
+    found_sets_this_cycle = 1
+
+
+    for index, row in data.iterrows():
+        for person in row['Customers']:
+            transaction_count.add(person)
+    
+    total = len(transaction_count)
+    items_list = data['Item'].tolist()
+
+    minimum = math.ceil(total*minimum_support)
+    
+    for index, rows in data.iterrows():
+        cust = rows['Customers']
+        if len(cust) > minimum:
+            frequent_items[rows['Item']] = set(rows['Customers'])
+          
+
+
+    if len(frequent_items) == 0:
+        return -1
+    
+    supported_sets.update(frequent_items)
+
+    while found_sets_this_cycle != 0:
+        n_size+=1
+        found_sets_this_cycle = 0
+        set_candidate = list(combinations(frequent_items, n_size))
+        found_sets = {}
+
+        for itemset in set_candidate:
+            set_pool = []
+            for item in itemset:
+                set_pool.append(frequent_items[item])
+            intersects = set.intersection(*set_pool)
+            if len(intersects) >= minimum:
+
+                found_sets[itemset] = intersects
+                found_sets_this_cycle+=1
+
+        
+        if found_sets_this_cycle!= 0:
+            found_items = []
+            supported_sets.update(found_sets)
+            for sets in found_sets:
+                for items in sets:
+                    found_items.append(items)
+            found_items =  list(set(found_items))
+   
 
 
 
+    normalized_sets = {}
+    for k, v in supported_sets.items():
+        if isinstance(k, str):
+            normalized_sets[(k,)] = v  # wrap single string in a tuple
+        else:
+            normalized_sets[k] = v
 
-data = preprocessing.clean_data(data)
-data = to_vertical(data)
-formulas.support_vertical(data, {'bread', 'butter'})
+    supported_sets = normalized_sets  # replace the old dictionary
+
+    ret = formulas.generate_all_rules_eclat(minimum_confidence, supported_sets, total)
+
+    end_time = time.time()
+    elapsed_time_ms = (end_time - start_time) * 1000
+    print(f'Eclat completed in {elapsed_time_ms} ms')
+    return ret
+
+
+        
--- src/algorithms/formulas.py
+++ src/algorithms/formulas.py
@@ -6,20 +6,29 @@ products = pd.read_csv('../../data/products.csv')
 #Assotiation Rule Class, -1 means unknown/not calculated
 #Try to keep even first and second as lists even if 1 item
 class AssociationRule:
-    def __init__(self, first, second, data, isVertical = False):
-        self.first = first
-        self.second = second
-        if isVertical == False:
-            self.support = support(data, self)
-            self.confidence = confidence(data, self)
-            self.lift = lift(data, self)
+    def __init__(self, first, second, data=0, isVertical = False, total = 0):
+        
+        if isinstance(first, str):
+            self.first = {first}
         else:
-            self.support = support_vertical(data, self)
-            self.confidence = confidence(data, self, True)
-            self.lift = lift(data, self, True)
+            self.first = set(first)
+        
+        if isinstance(second, str):
+            self.second = {second}
+        else:
+            self.second = set(second)
+        
+        if isVertical == False:
+            self.support = support_apiori(data, self)
+            self.confidence = confidence_apiori(data, self)
+            self.lift = lift_apiori(data, self)
+        else:
+            self.support = support_eclat(self, data, total)
+            self.confidence = confidence_eclat(self, data, total)
+            self.lift = lift_eclat(self, data, True)
 
     def __str__(self):
-        return f'{self.first} -> {self.second}\n'
+        return f'{self.first} -> {self.second}\n, Support {self.support}, Confidence {self.confidence}'
     
     def __eq__(self, other):
         return self.first == other.first and self.second == other.second
@@ -30,27 +39,25 @@ class AssociationRule:
     def __repr__(self):
         return self.__str__()
     
+    
+    
 
 
-def confidence(data, rule: AssociationRule, isVertical=False):
-    if isVertical == False:
-        sup_dividend = support(data, rule)
-        sup_divisor = support(data, rule.first)
-    else:
-        sup_dividend = support_vertical(data, rule)
-        sup_divisor = support_vertical(data, rule.first)
+def confidence_apiori(data, rule: AssociationRule, isVertical=False):
+
+    sup_dividend = support_apiori(data, rule)
+    sup_divisor = support_apiori(data, rule.first)
 
     if sup_divisor == 0:
          return 0
     return sup_dividend / sup_divisor
 
 
-def lift(data, rule: AssociationRule, isVertical=False):
-    conf_dividend = confidence(data, rule)
-    if isVertical == False:
-        sup_divisor = support(data, rule.second)
-    else:
-        sup_divisor = support_vertical(data, rule.second)
+def lift_apiori(data, rule: AssociationRule, isVertical=False):
+
+
+    conf_dividend = confidence_apiori(data, rule)
+    sup_divisor = support_apiori(data, rule.second)
 
 
     if sup_divisor == 0:
@@ -58,13 +65,13 @@ def lift(data, rule: AssociationRule, isVertical=False):
     return conf_dividend / sup_divisor
           
 #Accepts both rules and itemsets
-def support(data, itemset):
+def support_apiori(data, itemset):
     amount = 0
     total = data.shape[0]
     not_found = False
 
     if isinstance(itemset, AssociationRule):
-         itemset = list(itemset.first) + list(itemset.second)
+        itemset = list(itemset.first) + list(itemset.second)
     if isinstance(itemset, str):
         itemset = [itemset]
 
@@ -78,38 +85,41 @@ def support(data, itemset):
             amount += 1
     return amount / total
 
-#Use only for vertical sets, rules can go to the other one
-def support_vertical(data, itemset):
-    transaction_count = set()
 
-    if isinstance(itemset, str):
-        itemset = [itemset]
+def support_eclat(itemset: tuple, found_sets: dict, total: int ) -> float:
+    if isinstance(itemset, AssociationRule):
+        itemset = tuple(itemset.first | itemset.second)
+    else:
+        itemset = tuple(itemset)
 
-    for index, row in data.iterrows():
-        for person in row['Customers']:
-            transaction_count.add(person)
+    itemset = tuple(sorted(itemset))
+    dividend = len(found_sets[itemset])
     
-    total = len(transaction_count)
+    return dividend/total
 
-    itemset = list(itemset)
-    customer_list = []
-    for items in itemset:
-        row = data[data['Item'] == items]
-        customer_list.append(set(row.iloc[0]['Customers']))
+def confidence_eclat(rule: AssociationRule, found_sets: dict, total: int) -> float:
+    dividend = rule.support
+    divisor = support_eclat(rule.first, found_sets, total)
     
-    top = set.intersection(*customer_list)
-    return len(top)/total
+    if divisor == 0:
+        return 0
+    else:
+        return dividend / divisor
 
-   
-        
+def lift_eclat(rule: AssociationRule, found_sets: dict, total: int) -> float:
 
-    
+    dividend = rule.confidence
+    divisor = support_eclat(rule.second, found_sets, total)
+
+    if divisor == 0:
+        return 0
+
+    return dividend/divisor
 
 
-        
 
 
-def generate_all_rules(itemsets, minimum_confidence, data, isVertical=False):
+def generate_all_rules_apiori(itemsets, minimum_confidence, data):
     all_sets = set()
     possible_rules = set()
     real_rules = set()
@@ -121,36 +131,50 @@ def generate_all_rules(itemsets, minimum_confidence, data, isVertical=False):
 
     for sets in all_sets:
         if len(sets) == 2:
-
             a, b = tuple(sets)
-            if isVertical == False:
-                possible_rules.add(AssociationRule({a}, {b}, data))
-                possible_rules.add(AssociationRule({b}, {a}, data))
-            else:
-                possible_rules.add(AssociationRule({a}, {b}, data, True))
-                possible_rules.add(AssociationRule({b}, {a}, data, True))
-
+            possible_rules.add(AssociationRule({a}, {b}, data))
+            possible_rules.add(AssociationRule({b}, {a}, data))
         else:
-
-            
             i = 2
 
             while i < len(sets):
                 for combo in combinations(sets, i):
                     first = set(combo)
                     second = set(sets) - first
-                    
-                    if isVertical == False:
-                        possible_rules.add(AssociationRule(first, second, data))
-                        possible_rules.add(AssociationRule(second, first, data))
-                    else:
-                        possible_rules.add(AssociationRule(first, second, data, True))
-                        possible_rules.add(AssociationRule(second, first, data, True))
                 
+                    possible_rules.add(AssociationRule(first, second, data))
+                    possible_rules.add(AssociationRule(second, first, data))
                 i+=1
             
         for rules in possible_rules:
             if rules.confidence >= minimum_confidence:
                 real_rules.add(rules)
         
+    return real_rules
+
+def generate_all_rules_eclat(minimum_confidence, found_sets, total):
+    real_rules = set()
+    possible_rules = set()
+
+    for sets in found_sets:
+        possible_rules = set()
+        if len(sets) == 2:
+        
+            possible_rules.add(AssociationRule(sets[0], sets[1], isVertical=True, total=total, data=found_sets))
+            possible_rules.add(AssociationRule({sets[1]}, {sets[0]}, isVertical=True, total=total, data=found_sets))
+        elif len(sets) > 2:
+            i = 2
+            while i < len(sets):
+                for combo in combinations(sets, i):
+                    first = set(combo)
+                    second = set(sets) - first
+                
+                    possible_rules.add(AssociationRule(first, second, isVertical=True, total=total, data=found_sets))
+                    possible_rules.add(AssociationRule(second, first, isVertical=True, total=total, data=found_sets))
+                i+=1
+
+        for rules in possible_rules:
+            if rules.confidence >= minimum_confidence:
+                real_rules.add(rules)
+    
     return real_rules
